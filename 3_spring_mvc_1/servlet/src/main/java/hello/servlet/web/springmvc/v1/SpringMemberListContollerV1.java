978512771ffb096d93fec9f513a505e6bf630e0f
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-07-21 23:17:38 +0900

    23/07/21
    spring mvc 매핑 까지

---
+++ 3_spring_mvc_1/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListContollerV1.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+public class SpringMemberListContollerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        
+        mv.addObject("members", members);
+        return mv;
+    }
+}
